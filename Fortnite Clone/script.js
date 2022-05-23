3b3b70fa6b2435e142c89f9ab80d5ed2550d086f
RAHULBAWA <rahulbawa514@gmail.com>
2022-05-23 22:50:27 +0530

    Fortnite Clone files added with readme

---
+++ Fortnite Clone/script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
