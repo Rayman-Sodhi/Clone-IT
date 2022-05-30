a1026a23efca9d69b16ffe58937ddf958a42a9d3
Aarya Ahjolia <aaryaahjolia282@gmail.com>
2022-05-30 11:06:43 +0530

    Tesla Reactjs

---
+++ Tesla Clone ReactJS/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
