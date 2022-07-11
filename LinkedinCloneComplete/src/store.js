4df6a3083b6ba22b3546085deac4f8f1b0f568c0
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-07-11 22:58:12 +0530

    Delete LinkedinCloneComplete directory

--- LinkedinCloneComplete/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../features/userSlice';
-
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
