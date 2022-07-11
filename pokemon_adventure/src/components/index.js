5fd907bde69e608d0e71a41c03e012b0865ea48a
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-07-11 23:14:15 +0530

    Delete pokemon_adventure directory

--- pokemon_adventure/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Modal } from "./Modal/Modal";
-export { default as Search } from "./Search/Search";
-export { default as SinglePokemon } from "./SinglePokemon/SinglePokemon";
-export { default as Pokemons } from "./Pokemons/Pokemons";
--- pokemon_adventure/src/constants/actionTypes.js
+++
@@ -1,4 +0,0 @@
-// Pokemons Types
-export const FETCH_POKEMONS = "FETCH_POKEMONS";
-export const FETCH_POKEMON = "FETCH_POKEMON";
-export const FETCH_POKEMONS_BY_TYPE = "FETCH_POKEMONS_BY_TYPE";
