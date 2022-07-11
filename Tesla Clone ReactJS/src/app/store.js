4755e388bbb1e8973e246df68ab41434336c4905
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-07-11 23:06:54 +0530

    Delete Tesla Clone ReactJS directory

--- Tesla Clone ReactJS/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import carReducer from '../features/car/carSlice';
-
-export const store = configureStore({
-  reducer: {
-    car: carReducer,
-  },
-});
