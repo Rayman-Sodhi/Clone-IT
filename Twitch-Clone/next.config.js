ca30a5ea946ee5d88344ee5de8ff7868f9088018
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-07-12 21:52:02 +0530

    Delete Twitch-Clone directory

--- Twitch-Clone/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['static.twitchcdn.net', 'images-na.ssl-images-amazon.com'],
-  }
-}
-
-module.exports = nextConfig
